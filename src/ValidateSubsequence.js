92121903ee03268061f6fef94cd3531d9df5f488
IBNU FAUZI MEIRIYANTO <19058263@MAC2UBJHCDITCD.local>
2022-09-17 17:18:56 +0700

    add test and descriptionz'

--- src/ValidateSubsequence.js
+++ src/ValidateSubsequence.js
@@ -1,6 +1,8 @@
 class ValidateSubsequence {
 
   static isValidSubsequence(array, sequence) {
+    // Time Complexcity = O(n) Linear time complexity.
+    // Just need 1 loop to process and n based on length of array parameter
   	let indexSequence = 0
     for(let index = 0; index  < array.length; index++) {
       if(array[index] === sequence[indexSequence]) {
--- test/ValidateSubsequence.test.js
+++ test/ValidateSubsequence.test.js
@@ -10,5 +10,23 @@ describe('ValidateSubsequence', () => {
 
 			expect(actualResult).toBeTruthy()
 		})
+
+		it('2. should return true', () => {
+			const inputArray = [5, 1, 22, 25, 6, -1, 8, 10];
+			const inputSequence =  [5, 1, 22, 25, 6, -1, 8, 10];
+
+			const actualResult = ValidateSubsequence.isValidSubsequence(inputArray, inputSequence)
+
+			expect(actualResult).toBeTruthy()
+		})
+
+		it('3. should return true', () => {
+			const inputArray = [5, 1, 22, 25, 6, -1, 8, 10];
+			const inputSequence =  [5, 1, 22, 6, -1, 8, 10];
+
+			const actualResult = ValidateSubsequence.isValidSubsequence(inputArray, inputSequence)
+
+			expect(actualResult).toBeTruthy()
+		})
 	})
 })
